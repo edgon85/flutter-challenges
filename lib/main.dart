d577ec5a66b644065aaa4d1d8c97fcf7f6df3b86
Edwin Gonzalez <hgedwin@wedevgt.com>
2021-06-06 12:00:45 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:challenges/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Challeges',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => HomePage()
+      },
+    );
+  }
+}
