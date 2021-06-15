c614b69e4546045ba2970749bfdc66b6e886ec9b
Edwin Gonzalez <hgedwin@wedevgt.com>
2021-06-15 10:37:49 -0600

    widget expanded

--- lib/src/pages/expanded_page.dart
+++ lib/src/pages/expanded_page.dart
@@ -7,10 +7,29 @@ class ExpandedPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Expanded'),
       ),
-      body: Container(
-        child: Center(
-          child: Text('Expanded widget'),
-        ),
+      body: Row(
+          children: <Widget>[
+            Cuadrado(color: Colors.amber),
+            Expanded(flex: 3, child: Cuadrado(color: Colors.red)),
+            Expanded(flex: 2, child: Cuadrado(color: Colors.red)),
+            Cuadrado(color: Colors.greenAccent),
+          ],
+        ));
+  }
+}
+
+class Cuadrado extends StatelessWidget {
+  final Color color;
+
+  const Cuadrado({required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+      width: 50.0,
+      height: 50.0,
+      decoration: BoxDecoration(color: this.color
       ),
     );
   }
